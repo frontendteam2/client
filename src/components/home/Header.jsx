6fd5900622907d43edb02ba1ec50cfd2ef1c971a
Junyoung Ho <holee1213@naver.com>
2023-12-28 18:17:20 +0900

    feat:header component, home page

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const navigate=useNavigate()
+
   return (
-    <header>
-      
+    <header className="max-sm:hidden bg-slate-300">
+    <nav className="cursor-pointer" onClick={()=>{navigate('/')}}>aaaaa</nav>  
     </header>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
+import { useEffect,useState } from "react";
+import {useMaxWidth} from '../util/useMaxWidth'
 
+export default function Home() {
 
-export default function Home(){
-
-  return(
-    <main>
-
+  const width=useMaxWidth()
+  
+  return (
+    <main className={`${width?"max-w-[1200px] mx-auto":"w-full whitespace-nowrap"} text-gray-500 border`}>
+      <section>
+        <h1>
+          <div className={`${width?"text-6xl ":"text-2xl"} text-center border`}>
+            행사를 알리는<br/>가장 쉬운 방법
+          </div>
+          <div className={`${width?"text-8xl ":"text-4xl"} text-center text-black font-bold`}>여기서 만나요</div>
+        </h1>
+        <div>지금 바로 만들어보세요!</div>
+      </section>
+      
     </main>
-  )
+  );
 }
