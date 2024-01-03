680744db384adf184d87f89209e20564ada71327
tpdud0544@gmail.com <tpdud0544@gamil.com>
2024-01-03 18:38:08 +0900

    feat view ui

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react"
 import { IoIosArrowDown } from 'react-icons/io';
 import { MdContentCopy } from 'react-icons/md';
-
+import { IoMdArrowDroprightCircle } from "react-icons/io";
+import { useMaxWidth } from "../util/useMaxWidth";
 
 export default function View(){
   let [modal, setModal] = useState(false)
+  const width = useMaxWidth();
 
   //주소복사
 
@@ -18,12 +20,13 @@ export default function View(){
   };
 
   return(
-    <div className="w-3/5 m-auto ">
+  <div className={`${width ? " " : "min-w-[400px]"} w-1/2 p-10 mx-auto font-['JalnanGothic']`}>
+  {/* // <div className={`min-w-[150px] max-w-[300px] ${width?'mx-[30px]':'m-[5px]'} w-1/2 mx-auto font-['JalnanGothic']`}> */}
       <div className="box1 w-full flex mt-3">
-        <div className="inline float-left font-bold ">
-            <h2 className="text-3xl inline-flex">해빗팩토리</h2>
-            <div className=" text-neutral-400 text-sm mt-2">
-              <p className="float-left ">지번 주소 보기</p>
+        <div className="inline float-left ">
+            <h2 className="text-3xl inline-flex font-['JalnanGothic']">해빗팩토리</h2>
+            <div className=" text-neutral-400 text-sm mt-1">
+              <p className="float-left">지번 주소 보기</p>
               <IoIosArrowDown className= "text-xl" onClick={()=>{setModal(!modal)}}/>
             </div>
         </div>
@@ -34,16 +37,16 @@ export default function View(){
       <div className="clear-both"></div>
 
         { modal == true ? ( 
-          <ul className="w-40 h-50 p-2 absolute bg-white text-s font-bold border-2 rounded-xl">
-            <li className="p-1 text-sm">도로명 주소 보기</li>
+          <ul className="w-40 h-50 p-2 absolute bg-white text-s  border-2 rounded-xl">
+            <li className="p-1 text-[.8rem]">도로명 주소 보기</li>
             <li><hr /></li>
-            <li className="p-1 text-sm">지번 주소 보기</li>
+            <li className="p-1 text-[.8rem]">지번 주소 보기</li>
           </ul>
           ) : null
         }
 
-        <div className="w-full h-20  bg-gray-200 border-none rounded-3xl flex p-7 mt-3">
-          <p className="copy font-bold text-l">서울 강남구 역삼로25길 36 (역삼동)</p>
+        <div className={"w-full h-20  bg-gray-200 border-none rounded-3xl flex p-7 mt-3 "}>
+          <p className={"copy text-[0.9rem] mt-1"}>서울 강남구 역삼로25길 36 (역삼동)</p>
           <div className="flex-grow"></div>
           <MdContentCopy className="mt-1" onClick={()=>{handleCopyClipBoard('복사')}}/>
         </div>
@@ -53,37 +56,49 @@ export default function View(){
         </div>
 
         <div className="border-b-2 border-gray-100 mt-5 mb-5">
-          <p className="font-bold">사진보기</p>
+          <div>
+            <IoMdArrowDroprightCircle className="float-left mr-1" />
+            <p className="text-[.85rem] ">사진보기</p>
+          </div>
           <img className="w-36 h-36 object-cover float-left pr-2" src="https://juso-io.s3.ap-northeast-2.amazonaws.com/upload/63_4e495b68-18c3-466f-bb6a-a2c8fc9d18a1.jpeg" alt="" />
-          <img className="w-36 h-36 object-cover mb-3 pr-2" src="https://juso-io.s3.ap-northeast-2.amazonaws.com/upload/63_4e495b68-18c3-466f-bb6a-a2c8fc9d18a1.jpeg" alt="" />
+          <div className="w-36 h-36 object-cover mb-3 pr-2"></div>
         </div>
 
         <div className="border-b-2 border-gray-100 mt-7">
-          <p className="font-bold">오시는 방법</p>
-          <p className="w-full text-sm p-2">
+          <div>
+            <IoMdArrowDroprightCircle className="float-left mr-1" />
+            <p className="text-[.85rem]">오시는 길</p>
+          </div>
+          {/* <p className="w-full text-[.8rem] font- p-2 text-neutral-500 font-['SUITE-Regular'] font-bold">
+            건물 외벽에 시그널플래너 로고를 찾으시면 됩니다.
+          </p> */}
+          <p className="w-full text-[.75rem] font- p-2 text-neutral-500 font-['Nanum Gothic Coding']">
             건물 외벽에 시그널플래너 로고를 찾으시면 됩니다.
           </p>
+
         </div>
 
         <div className="border-b-2 border-gray-100 mt-7">
-          <p className="font-bold">무료주차장</p>
-          <p className="w-full text-sm p-2">
+          <div>
+            <IoMdArrowDroprightCircle className="float-left mr-1" />
+            <p className="text-[.85rem]">무료주차장</p>
+          </div>
+          <p className="w-full text-[.75rem] font- p-2 text-neutral-500 font-['Nanum Gothic Coding']">
             건물 1층에 무료 주차 가능합니다.
           </p>
         </div>
 
         <div className="border-b-2 border-gray-100 mt-7">
-          <p className="font-bold">다른 앱에서 보기</p>
-        </div>
-
-        <div className="border-b-2 border-gray-100 mt-7">
-          <p className="font-bold">알리고 싶은 추가 정보</p>
-          <p className="w-full text-sm p-2">
+          <div>
+            <IoMdArrowDroprightCircle className="float-left mr-1" />
+            <p className="text-[.85rem]">알리고 싶은 추가정보</p>
+          </div>
+          <p className="w-full text-[.75rem] font- p-2 text-neutral-500 font-['Nanum Gothic Coding']">
             해빗팩토리 소개
           </p>
         </div>
 
-        <button type="button" className="font-bold block p-3 m-auto mt-10 bg-gray-200 border-none rounded-2xl">
+        <button type="button" className=" block p-3 m-auto mt-10 bg-gray-200 border-none rounded-2xl text-[.8rem]">
           나도 만들기
         </button>
 
