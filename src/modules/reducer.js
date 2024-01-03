883b34868bd142086738fb1ab284a375e86e2d54
음정태 <dmawjdxo12@naver.com>
2024-01-03 18:25:16 +0900

    refactor  redux

--- src/modules/reducer.js
+++ src/modules/reducer.js
@@ -1,17 +1,24 @@
-import Question from '../components/home/Content';
+import Question from '../components/content/Content';
 
 
-const items = { key: 1, question: [] }
+const items =  [] 
+
 export default function reducer(state = items, action) {
-  if (action.type === 'add') {
-      return { key: state.key + 1, question: [...state.question, <Question key={state.key} num={state.key} txt={action.title} />] }
+  if (action.type === 'content') {
+    return [...state,{type :action.type,title : action.title}]
+    // return { key: state.key + 1, question: [...state.question, <Question key={state.key} num={state.key} txt={action.title} />] }
   }
-  else if(action.type === 'image'){
+  else if (action.type === 'image') {
     console.log(action.type);
     return state
-  }else if(action.type === 'address'){
+  } else if (action.type === 'address') {
     console.log(action.type);
     return state
+  } else if (action.type === 'close') {
+    console.log(action,state);
+    // const updatedQuestions = [...state];
+    // updatedQuestions.splice(action.num, 1);
+    return state.filter((v,i)=> i !== action.num )
   }
 
   return state
