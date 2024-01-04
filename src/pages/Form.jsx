08643b332845949061bbf11a191dac1d420a7e00
음정태 <dmawjdxo12@naver.com>
2024-01-04 18:20:12 +0900

    feat : form data 서버 연동

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,26 +1,28 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 // import Question from '../components/home/Content';
 import { useDispatch, useSelector } from "react-redux";
 import Content from "../components/content/Content";
 import { useMaxWidth } from "../util/useMaxWidth";
+import ImgUpload from './../components/ImgUpload/ImgUpload';
+import SearchAddr from './../components/searchaddr/SearchAddr';
+import axios from "axios";
 
 export default function Form() {
-  // const [items, setItems] = useState([]);
   const [questionTitle, setQuestionTitle] = useState('');
   const [btnToggle, setBtnToggle] = useState(false);
-  // const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  let [detailForm, setDetailForm] = useState([]);
   const width = useMaxWidth();
 
   let state = useSelector(state => state)
-  const dispatch = useDispatch();
-  // useEffect(() => {
-  //   const resizeListener = () => {
-  //     setInnerWidth(window.innerWidth);
-  //   };
-  //   window.addEventListener("resize", resizeListener);
-  // });
 
-  // console.log("innerWidth", width);
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    // state가 변경될 때마다 detailForm을 업데이트
+    setDetailForm([...state]);
+  }, [state]); // state가 변경될 때만 useEffect가 실행되도록 설정
+  // 나머지 컴포넌트 로직...
 
   const addItem = (txt) => {
     if (txt === 'image') {
@@ -37,13 +39,51 @@ export default function Form() {
 
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userConfirmed = window.confirm('하실?.');
+    if (userConfirmed) {
+      const formData = new FormData(e.target);
+      const formDataObject = {};
+      let i = 0;
+      formData.forEach((value, key) => {
+        if (key.includes('content')) {
+          setDetailForm([...detailForm, detailForm[i].content = value])
+          i++
+        } else if (key.includes('image')) {
+          setDetailForm([...detailForm, detailForm[i].image = value])
+          i++
+        } else if (key.includes('address')) {
+          setDetailForm([...detailForm, detailForm[i].address = value])
+          i++
+        } else {
+          formDataObject[key] = value;
+        }
+      });
+      axios({
+        method: 'post',
+        url: `http://127.0.0.1:8000/newForm`,
+        data: [formDataObject, detailForm],
+
+
+      })
+        .then(result => {
+
+        })
+        .catch(err => console.log('에러==>' + err))
+    }
+
+
+  }
+
   return (
     <main className="min-h-[100vh] mb-24">
-      <form className={`${width ? 'w-[50%]' : 'w-[90%]'} mx-auto block`}>
+      <form onSubmit={handleSubmit} className={`${width ? 'w-[50%]' : 'w-[90%]'} mx-auto block`}>
         <fieldset className="mx-auto w-[100%] box-border block">
 
 
-          <h2 className="mt-28 text-lg">주소 추가하기</h2>
+          <h2 className="mt-28 text-2xl font-bold">주소 추가하기</h2>
           <p className="text-sm mt-10 font-bold text-stone-900">
             공유할 때 사용할 내 주소 페이지의 URL을 입력해주세요. <br />
             한글, 영문 모두 사용 가능하며 모두 동일한 페이지로 이동합니다
@@ -53,13 +93,13 @@ export default function Form() {
           <div className="mt-5 rounded-xl bg-stone-200">
             <div className="block flex text-center ">
               <label htmlFor="" className={`w-[30%] box-border px-3 py-4 text-stone-900  ${width ? 'text-sm' : 'text-xs'} `}>http://localhost/</label>
-              <input type="text" name='url' placeholder="url을 입력해주세요." className="rounded-xl text-sm py-4 block box-border w-[70%]  bg-stone-50 px-2 box-border" /></div>
+              <input type="text" maxLength='20' name='url' placeholder="url을 입력해주세요." className="rounded-xl text-sm py-4 block box-border w-[70%]  bg-stone-50 px-2 box-border" /></div>
           </div>
 
           <div className="text-sm mt-12 text-stone-900 font-semibold block">페이지 제목을 입력해주세요</div>
           <div className="mt-5 rounded-xl bg-stone-200">
             <div className="block flex text-center ">
-              <input type="text" name='title' placeholder="페이지 제목을 입력해주세요." className="rounded-xl text-sm py-4 block flex-1 bg-stone-50 px-2 box-border" />
+              <input type="text" name='title' maxLength='20' placeholder="페이지 제목을 입력해주세요." className="rounded-xl text-sm py-4 block flex-1 bg-stone-50 px-2 box-border" />
             </div>
           </div>
 
@@ -67,21 +107,27 @@ export default function Form() {
           {state && state.map((v, i) => {
             if (v.type === 'content') {
               return <Content key={i} num={i} txt={v.title} />
+            } else if (v.type === 'image') {
+              return <ImgUpload key={i} num={i} />
+            } else if (v.type === 'address') {
+              return <SearchAddr key={i} num={i} />
             }
           })}
-          <div className={`${width ? 'fixed top-80 right-40 w-[250px]' : 'static w-[100%] mt-10' } `} >
-            <button type="button" className={`w-[100%] px-4 py-2 box-border bg-neutral-400 font-bold rounded-md text-white`} onClick={() => setBtnToggle(!btnToggle)}>Add</button>
+          <div className={`${width ? 'fixed top-80 right-40 w-[250px]' : 'static w-[100%] mt-10'} `} >
+            <button type="button" className={`w-[100%] px-4 py-2 box-border hover:bg-neutral-500 bg-neutral-400 font-bold rounded-md text-white`} onClick={() => setBtnToggle(!btnToggle)}>Add</button>
             <ul>
               {btnToggle &&
                 <>
-                  <li className="mt-3 py-1  bg-neutral-400 text-center text-white rounded-md cursor-pointer" onClick={() => addItem('image')}>이미지 추가+</li>
-                  <li className="mt-3 py-1  bg-neutral-400 text-center text-white rounded-md cursor-pointer" onClick={() => addItem('address')}>주소 추가+</li>
+                  <li className="mt-3 py-1  bg-neutral-400 hover:bg-neutral-500 text-center text-white rounded-md cursor-pointer" onClick={() => addItem('image')}>이미지 추가+</li>
+                  <li className="mt-3 py-1  bg-neutral-400 hover:bg-neutral-500 text-center text-white rounded-md cursor-pointer" onClick={() => addItem('address')}>주소 추가+</li>
                   <li className="flex mt-3">
-                    <input type="text" className="w-[75%] block bg-stone-100 box-border px-2" value={questionTitle} onChange={(e) => setQuestionTitle(e.target.value)} />
-                    <button type="button" className="w-[25%] block text-sm align-middle  py-1 bg-neutral-400  rounded-md text-white" onClick={() => addItem('content')}>추가+</button>
+                    <input type="text" maxLength='10' className="w-[75%] block bg-stone-100 box-border px-2" value={questionTitle} onChange={(e) => setQuestionTitle(e.target.value)} />
+                    <button type="button" className="w-[25%] block text-sm align-middle  py-1 bg-neutral-400 hover:bg-neutral-500  rounded-md text-white" onClick={() => addItem('content')}>추가+</button>
                   </li>
                 </>
               }
+              <button className="mt-3 py-1 w-[100%]  bg-neutral-400 hover:bg-neutral-500 py-2  text-center text-white font-bold  rounded-md cursor-pointer" >저장 하기</button>
+
             </ul>
           </div>
         </fieldset>
@@ -93,20 +139,3 @@ export default function Form() {
   );
 }
 
-/* 
-[[category,[title,content]],[category=picture],[url,~,~,~~],[location,[]] ]
-
-question.map((v,i=>{
-//v=배열
-
-if(v[0]==content) return <content param num=i~~~~~>
-else (v[0]==picture) return
-
-
-
-
-})
-
-
-
-*/
